d2d045958be374b80eb7a11b1a468f4d78081650
Mason Mackaman <masondeanm@aol.com>
2023-02-15 03:18:11 -0500

    Add preventDefault to Lude

--- src/Lude.purs
+++ src/Lude.purs
@@ -30,6 +30,7 @@ import Halogen (Component, ComponentHTML, HalogenM, SubscriptionId) as Exports
 import Halogen.VDom.Driver (runUI) as Exports
 import Halogen.HTML (HTML) as Exports
 import Halogen.HTML.Properties (IProp) as Exports
+import Web.Event.Event (preventDefault) as Exports
 
 import MasonPrelude
 import Data.Enum (class BoundedEnum, Cardinality(..), cardinality, toEnum)
